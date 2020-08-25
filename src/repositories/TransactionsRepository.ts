4614c85bf86f4a03f3bbb34ad0fc74b715a3e290
Diogo Sales <diogosalesdev@gmail.com>
2020-08-25 20:25:24 -0300

    Challenge accomplished

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,17 @@
+import { uuid } from 'uuidv4';
+
 import Transaction from '../models/Transaction';
 
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'outcome' | 'income';
 }
 
 class TransactionsRepository {
@@ -14,15 +22,53 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    // DONE
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    // DONE
+    const income = this.transactions.reduce((previous, current) => {
+      if (current.type === 'income') {
+        return previous + current.value;
+      }
+      return previous;
+    },0);
+
+    const outcome = this.transactions.reduce((previous, current) => {
+      if (current.type === 'outcome') {
+        return previous + current.value;
+      }
+      return previous;
+    },0);
+
+    const total = income - outcome;
+
+    return {
+      income,
+      outcome,
+      total
+    }
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: Request): Transaction {
+    // DONE
+    const { total } = this.getBalance();
+
+    if (total < value && type === 'outcome') {
+      throw Error('Insufficient funds!');
+    }
+
+    const transaction = {
+      id: uuid(),
+      title,
+      value,
+      type
+    }
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
